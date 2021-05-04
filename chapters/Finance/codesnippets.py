2a4bdc9ea19a7cd3ccb9441f35ace473aacd29b4
amoreira2 <alanmoreira@gmail.com>
2021-05-04 10:19:09 -0400

    data

--- chapters/Finance/codesnippets.py
+++ chapters/Finance/codesnippets.py
@@ -5,3 +5,20 @@ def SR(R,W=[],freq=12):
         r=R@W
         sr=r.mean()/r.std()*freq**0.5
     return sr
+
+def get_returns(tickers,conn,startdate,enddate,variable='ret'):
+    ticker=tickers[0]
+    df = conn.raw_sql("""
+                          select a.permno, b.ncusip, b.ticker, a.date, b.shrcd,
+                          a.ret, a.vol, a.shrout, a.prc, a.cfacpr, a.cfacshr,a.retx
+                          from crsp.dsf as a
+                          left join crsp.msenames as b
+                          on a.permno=b.permno
+                          and b.namedt<=a.date
+                          and a.date<=b.nameendt
+                          where a.date between '"""+ startdate+"""'  and '"""+ enddate+            
+                          """' and  b.ticker='"""+ticker+"'")
+            
+    df.set_index(['date','permno'],inplace=True)
+    df=df[variable].unstack()
+    return df
